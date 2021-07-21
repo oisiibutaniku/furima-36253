6bfc14ed8c3eaf3e7e7dbea741eab78a325dcbf2
oisiibutaniku <butanoosasimi@gmail.com>
2021-07-21 19:25:49 +0900

    ログイン、新規登録機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
-  
+  #before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  def after_sign_in_path_for(resource)
+    root_path
   end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
+  private
+  def configure_permitted_parameters
+ devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :encrypted_password, :c_family_name, :c_first_name, :k_family_name, :k_first_name, :birthday ])  
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    User.create(user_params)
+  end
+  
+
+  def edit
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+end

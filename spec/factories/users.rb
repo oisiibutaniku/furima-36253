af0f2e13767b0be44e0ec4b5ffb95422e12aba3f
oisiibutaniku <butanoosasimi@gmail.com>
2021-07-23 11:34:09 +0900

    修正１０

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.initials(number: 2)}
-    email                 {'test@example'}
+    email                 {Faker::Internet.free_email}
     password              {'000000a'}
     password_confirmation {'000000a'}
     c_family_name         {'田中'}

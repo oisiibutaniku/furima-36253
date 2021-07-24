26b8b578e4cc7267838341469126ed63d808d92f
oisiibutaniku <butanoosasimi@gmail.com>
2021-07-24 13:03:49 +0900

    投稿機能実装

--- Gemfile
+++ Gemfile
@@ -60,4 +60,8 @@ end
 gem 'devise'
 gem 'rspec-rails', '~> 4.0.0'
 gem 'factory_bot_rails'
-gem 'faker'
+gem 'faker' 
+
+gem 'mini_magick'
+gem 'image_processing', '~> 1.2'
+gem 'active_hash'
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,27 @@
 class Item < ApplicationRecord
+  
+  belongs_to :user
+  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :state
+  belongs_to :burden
+  belongs_to :prefecture
+  belongs_to :day
+
+
+validates :product, presence: true
+validates :explanation, presence: true
+validates :price, presence: true
+validates :image, presence: true
+validates :category_id, :state_id,:burden_id,:prefecture_id,:day_id, presence: true
+#ジャンルの選択が「--」の時は保存できないようにする
+validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
+validates :state_id, numericality: { other_than: 1 , message: "can't be blank"}
+validates :burden_id, numericality: { other_than: 1 , message: "can't be blank"}
+validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+validates :day_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+
 end
+

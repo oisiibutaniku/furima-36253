01520130b8a11f5e0d6f08d90b27886dcba8abbf
oisiibutaniku <butanoosasimi@gmail.com>
2021-07-24 21:16:22 +0900

    nodelのバリデーション作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,12 +9,13 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :day
 
-
-validates :product, presence: true
-validates :explanation, presence: true
-validates :price, presence: true
-validates :image, presence: true
-validates :category_id, :state_id,:burden_id,:prefecture_id,:day_id, presence: true
+  with_options presence: true do
+validates :product
+validates :explanation
+validates :price
+validates :image
+validates :category_id, :state_id,:burden_id,:prefecture_id,:day_id
+  end
 #ジャンルの選択が「--」の時は保存できないようにする
 validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
 validates :state_id, numericality: { other_than: 1 , message: "can't be blank"}
@@ -22,6 +23,7 @@ validates :burden_id, numericality: { other_than: 1 , message: "can't be blank"}
 validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
 validates :day_id, numericality: { other_than: 1 , message: "can't be blank"}
 
-
+validates :price, inclusion: { in: 300..9_999_999, message: "is out of setting range"}
+validates :price, numericality: { only_integer: true,message: "is invalid. Input half-width characters"} 
 end
 

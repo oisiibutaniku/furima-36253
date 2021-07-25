ccd681a041b1b24da5caba26ec0268636dbcd473
oisiibutaniku <butanoosasimi@gmail.com>
2021-07-25 12:05:05 +0900

    テストコードの入力

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_23_061200) do
+ActiveRecord::Schema.define(version: 2021_07_23_051317) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false

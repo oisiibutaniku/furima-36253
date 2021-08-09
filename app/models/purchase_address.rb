2d8aefe67d928b7738039b29877d7638245aaea5
oisiibutaniku <butanoosasimi@gmail.com>
2021-08-09 10:40:10 +0900

    修正３

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -6,15 +6,13 @@ class PurchaseAddress
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
     validates :city
     validates :street
-    validates :phone
     validates :token
     validates :user_id
     validates :item_id
     validates :phone, length: {minimum:10, maximum:11}
-    validates :phone,format: {with: /\A[0-9]+\z/, message: "number is invalid. Input only number"}
   end
   validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"} 
-
+  validates :phone,format: {with: /\A[0-9]+\z/, message: "number is invalid. Input only number"}
     
 
 
